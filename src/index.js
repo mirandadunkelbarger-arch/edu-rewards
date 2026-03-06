7ede8579602cbbfde5a536d5cb463b9078807472
mirandadunkelbarger-arch <miranda.dunkelbarger@gmail.com>
2026-03-05 21:53:35 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import './index.css';
+import App from './App'; // <-- This line looks for App.js in the same folder
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
